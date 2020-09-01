62d3d134c5b15e0f2f7ecc6336e5644fed817905
manishsurolia <manishsurolia@gmail.com>
2020-09-01 11:01:13 +0530

    code related to LCA in a tree.

---
+++ tree/lowest_common_ancestor/lca.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tree
+{
+    struct tree * left;
+    struct tree * right;
+    int data;
+};
+
+struct tree * add_node(struct tree *p, int data)
+{
+    if(p == NULL) {
+        p = (struct tree *) malloc (sizeof(struct tree));
+        p->data = data;
+        p->left = NULL;
+        p->right = NULL;
+    } else if(data < p->data) {
+        p->left = add_node(p->left, data);
+    } else {
+        p->right = add_node(p->right, data);
+    }
+    return p;
+}
+
+struct tree * LCA(struct tree *p, int data1, int data2)
+{
+    struct tree *left=NULL;
+    struct tree *right=NULL;
+    if(p == NULL) {
+        return NULL;
+    } else {
+        if((p->data == data1) || (p->data == data2)) {
+            return p;
+        } else {
+            left = LCA(p->left, data1, data2);
+            right = LCA(p->right, data1, data2);
+
+            if((left == NULL) && (right == NULL)) {
+                return NULL;
+            } else if(left !=NULL && right != NULL) {
+                return p;
+            } else {
+                return (left != NULL) ? left : right;
+            }
+        }
+    }
+}
+
+int main()
+{
+    struct tree * first = NULL;
+    struct tree * searched_node = NULL;
+    first = add_node(first, 40);
+    first = add_node(first, 45);
+    first = add_node(first, 35);
+    first = add_node(first, 20);
+    first = add_node(first, 90);
+    first = add_node(first, 10);
+    first = add_node(first, 95);
+    first = add_node(first, 5);
+    first = add_node(first, 7);
+    first = add_node(first, 88);
+
+    searched_node = LCA(first, 10, 5);
+    if(searched_node) {
+        printf("\nlca is : %d\n",searched_node->data);
+    } else {
+        printf("\nfound lca is NULL\n");
+    }
+
+    return 0;
+}
